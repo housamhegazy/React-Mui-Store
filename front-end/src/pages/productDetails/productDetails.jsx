39481efd1404e0445c7d6d71a3df55606cecdcd0
Housam Hegazy <geohousam@gmail.com>
2023-03-26 13:59:10 +0000

    sssssssssss

--- front-end/src/pages/productDetails/productDetails.jsx
+++ front-end/src/pages/productDetails/productDetails.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useGetoneproductsByNameQuery } from "../../Redux/products";
 import { useParams } from "react-router-dom";
+import 'productDetails.css';
+import DetailsThumb from "./productDetailsThumb";
+
 export default function ProductDetails() {
-    let userId = useParams();
-  const { data, error, isLoading } = useGetoneproductsByNameQuery(Number(userId.id));
+  let userId = useParams();
+  const { data, error, isLoading } = useGetoneproductsByNameQuery(
+    Number(userId.id)
+  );
+  const [index, setIndex] = useState(0);
+  const myRef = useRef(null)
+  const handleTab = (index) => {
+    setIndex(index);
+    const images = myRef.current.children;
+    for (let i = 0; i < images.length; i++) {
+      images[i].className = images[i].className.replace("active", "");
+    }
+    images[index].className = "active";
+  };
 
   if (data) {
-    console.log(data)
-    return <div>productDetails</div>;
+    return (
+      <div className="app productdetails">
+        {data.map((item) => (
+          <div className="details" key={item.id}>
+            <div className="big-img">
+              <img src={item.imageLink[item.index]} alt="" />
+            </div>
+
+            <div className="box">
+              <div className="row">
+                <h2>{item.productname}</h2>
+                <span>${item.price}</span>
+              </div>
+              {/* <Colors colors={item.colors} /> */}
+
+              <p>{item.description}</p>
+
+              <DetailsThumb images={item.imageLink} tab={handleTab} myRef={myRef} />
+              <button className="cart">Add to cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
