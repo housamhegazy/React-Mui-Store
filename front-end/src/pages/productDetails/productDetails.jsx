a58cdc9b5b46614e22fcfebeffc0775b509338af
housamhegazy <geohousam@gmail.com>
2023-03-26 17:49:11 +0300

    dxxxxxx

--- front-end/src/pages/productDetails/productDetails.jsx
+++ front-end/src/pages/productDetails/productDetails.jsx
@@ -1,16 +1,34 @@
 import React, { useRef, useState } from "react";
 import { useGetoneproductsByNameQuery } from "../../Redux/products";
 import { useParams } from "react-router-dom";
-import './productDetails.css';
+import "./productDetails.css";
 import DetailsThumb from "./productDetailsThumb";
+import { Remove, Add, ShoppingCart } from "@mui/icons-material";
+import { Stack, Badge, Button, styled } from "@mui/material";
+import { decreaseProducts, increaseProducts, addToCart } from "Redux/CartSlice";
+import { useDispatch, useSelector } from "react-redux";
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: "0 6px",
+  },
+}));
 
 export default function ProductDetails() {
   let userId = useParams();
   const { data, error, isLoading } = useGetoneproductsByNameQuery(
     Number(userId.id)
   );
+  const dispatch = useDispatch();
+  // @ts-ignore
+  const { insertedProducts } = useSelector((state) => state.counter);
+
+  const Quantity = (id) => {
+    const myproduct = insertedProducts.find((product) => product.id === id);
+    return myproduct.Quantity;
+  };
   const [index, setIndex] = useState(0);
-  const myRef = useRef(null)
+  const myRef = useRef(null);
   const handleTab = (index) => {
     setIndex(index);
     const images = myRef.current.children;
@@ -23,25 +41,65 @@ export default function ProductDetails() {
   if (data) {
     return (
       <div className="app productdetails">
-          <div className="details" key={data.id}>
-            <div className="big-img">
-              <img src={data.imageLink[index]} alt="" />
-            </div>
-
-            <div className="box">
-              <div className="row">
-                <h2>{data.productname}</h2>
-                <span>${data.price}</span>
-              </div>
-              {/* <Colors colors={data.colors} /> */}
-
-              <p>{data.description}</p>
-
-              <DetailsThumb images={data.imageLink} tab={handleTab} myRef={myRef} />
-              <button className="cart">Add to cart</button>
-            </div>
+        <div className="details" key={data.id}>
+          <div className="big-img">
+            <img src={data.imageLink[index]} alt="" />
           </div>
-        
+
+          <div className="box">
+            <div className="row">
+              <h2>{data.productname}</h2>
+              <span>${data.price}</span>
+            </div>
+            {/* <Colors colors={data.colors} /> */}
+
+            <p>{data.description}</p>
+
+            <DetailsThumb
+              images={data.imageLink}
+              tab={handleTab}
+              myRef={myRef}
+            />
+            {insertedProducts.find((product) => product.id === data.id) ? (
+              <Stack direction="row" sx={{ alignItems: "center" ,mt:3}}>
+                <Button
+                  size="small"
+                  onClick={() => {
+                    dispatch(decreaseProducts(data));
+                  }}
+                  sx={{ mx: 1 }}
+                >
+                  <Remove fontSize="small" />
+                </Button>
+                <StyledBadge
+                  badgeContent={Quantity(data.id)}
+                  color="secondary"
+                />
+                <Button
+                  size="small"
+                  onClick={() => {
+                    dispatch(increaseProducts(data));
+                  }}
+                  sx={{ mx: 1 }}
+                >
+                  <Add fontSize="small" />
+                </Button>
+              </Stack>
+            ) : (
+              <Button
+                onClick={() => {
+                  dispatch(addToCart(data));
+                }}
+                size="small"
+                sx={{ textTransform: "capitalize",mt:2 }}
+                variant="contained"
+              >
+                <ShoppingCart />
+                add to Basket
+              </Button>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
